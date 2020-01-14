21aed483b66874f7d9967f67d1e21574ac9ee319
RiggsRiggs <tom.riggs@webskyltd.com>
2020-01-14 16:31:09 +0000

    checks single URL

--- HeadCheckerTron/Program.cs
+++ HeadCheckerTron/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,15 @@ namespace HeadCheckerTron
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("check your head");
+            var url = "http://med04.expertagent.co.uk/in4glest ates/{7f8820d0-bb78-4105-8c70-ca5d23a41492}/{fa318b73-a054-41a6-b4ce-5c86b4e0b99a}/Photo1.jpg";
+            // create the request
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            // instruct the server to return headers only
+            request.Method = "HEAD";
+            // make the connection
+            var response = request.GetResponse() as HttpWebResponse;
+            // get the headers
+            Console.WriteLine(response);
         }
     }
 }
