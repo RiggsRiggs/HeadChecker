d2b104f5bf41d7b8691b6950040467fae475b50b
RiggsRiggs <tom.riggs@webskyltd.com>
2020-02-07 15:44:47 +0000

    Wayne added try catch, ready for loop

--- HeadCheckerTron/Program.cs
+++ HeadCheckerTron/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HeadCheckerTron
 {
@@ -11,15 +8,50 @@ namespace HeadCheckerTron
     {
         static void Main(string[] args)
         {
-            var url = "http://med04.expertagent.co.uk/in4glest ates/{7f8820d0-bb78-4105-8c70-ca5d23a41492}/{fa318b73-a054-41a6-b4ce-5c86b4e0b99a}/Photo1.jpg";
-            // create the request
-            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-            // instruct the server to return headers only
-            request.Method = "HEAD";
-            // make the connection
-            var response = request.GetResponse() as HttpWebResponse;
-            // get the headers
-            Console.WriteLine(response);
+            var url = "http://med04.expertagent.co.uk/in4glestates/{7f8820d0-bb78-4105-8c70-ca5d23a41492}/{fa318b73-a054-41a6-b4ce-5c86b4e0b99a}/Photo1.jpg";
+
+
+            CheckImage(url);
+
+            Console.Read();
+        }
+
+        private static void CheckImage(string url)
+        {
+            try
+            {
+                var request = WebRequest.Create(url);
+                request.Method = "HEAD";
+                using (var response = request.GetResponse())
+                {
+                    using (var responseStream = response.GetResponseStream())
+                    {
+                        Console.WriteLine("File Exists!");
+                        // Image exists
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError &&
+                    ex.Response != null)
+                {
+                    var resp = (HttpWebResponse)ex.Response;
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        // Mark image as not found!
+                        Console.WriteLine("Image NOT there!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"url:{url} - response: {resp.StatusCode} - {resp.StatusDescription}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"url:{url} - Error: {ex.Message}");
+                }
+            }
         }
     }
 }
